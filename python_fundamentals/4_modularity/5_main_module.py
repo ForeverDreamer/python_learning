8cc9e8e33067df4bcf2802bfddf249d6b479b341
doer <499361328@qq.com>
2019-05-01 11:33:49 +0800

    Revert "Revert "4_modularity""
    This reverts commit 07c415bce442b2685f96500c7e6c0143f6fbb453.

---
+++ python_fundamentals/4_modularity/5_main_module.py
@@ -0,0 +1,25 @@
+from urllib.request import urlopen
+
+
+def fetch_words():
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+
+    return story_words
+
+
+def print_words(words):
+    print(words)
+
+
+def main():
+    words = fetch_words()
+    print_words(words)
+
+
+if __name__ == '__main__':
+    main()
